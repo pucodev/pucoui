235b0b574338fb55d73a4209bc97416a41f55598
Jorge Estrada <jor1550g@gmail.com>
2026-02-04 15:21:30 -0500

    feat: add icon left to input field

--- apps/docs/src/components/html/form/input/InputIcon.jsx
+++ apps/docs/src/components/html/form/input/InputIcon.jsx
@@ -1,14 +1,26 @@
 export default function InputIcon() {
   return (
-    <div class="field is-invalid">
-      <label class="label">Password</label>
-      <div class="control">
-        <input class="input" type="password" placeholder="Password" />
-        <button class="btn input__icon">
-          <iconify-icon class="icon" icon="solar:eye-outline"></iconify-icon>
-        </button>
+    <div className="is-flex is-flex-column is-gap-3">
+      <div class="field">
+        <label class="label">Search</label>
+        <div class="control">
+          <iconify-icon
+            class="icon input__icon"
+            icon="solar:magnifer-outline"
+          ></iconify-icon>
+          <input class="input" type="text" placeholder="Search..." />
+        </div>
       </div>
-      <span class="help">Must have more than 6 characters</span>
+      <div class="field is-invalid">
+        <label class="label">Password</label>
+        <div class="control">
+          <input class="input" type="password" placeholder="Password" />
+          <button class="btn input__icon">
+            <iconify-icon class="icon" icon="solar:eye-outline"></iconify-icon>
+          </button>
+        </div>
+        <span class="help">Must have more than 6 characters</span>
+      </div>
     </div>
   )
 }
